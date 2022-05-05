a436b20fbfc2dd84c5a3e96332f4609c0e2128a0
Doan Quang Huy <quanghuybest@gmail.com>
2022-05-05 19:16:32 +0700

    Upload project mô hình 3 lớp (3 layer)

---
+++ QuanLyShopGiayDep/DAL/DBConnectData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DBConnectData
+    {
+        public static SqlConnection HamKetNoi()
+        {
+            SqlConnection conn = new SqlConnection(@"Chuỗi kết nối của sql server!!!");
+            return conn;
+        }
+    }
+}
