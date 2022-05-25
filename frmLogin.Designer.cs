9d336fdbc824dc43c75293f1df96fb72a97f9405
Doan Quang Huy <quanghuybest@gmail.com>
2022-05-25 10:44:57 +0700

    Cap nhat login form

---
+++ frmLogin.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace QuanLyShopGiayDep
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.txtTaiKhoan = new System.Windows.Forms.TextBox();
+            this.txtMatKhau = new System.Windows.Forms.TextBox();
+            this.lblTaiKhoan = new System.Windows.Forms.Label();
+            this.lblMatKhau = new System.Windows.Forms.Label();
+            this.pbHinh = new System.Windows.Forms.PictureBox();
+            this.lblTieuDe = new System.Windows.Forms.Label();
+            this.btnDangNhap = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.timeOut = new System.Windows.Forms.Timer(this.components);
+            this.chkLuuThongTin = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTaiKhoan
+            // 
+            this.txtTaiKhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtTaiKhoan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(18)))), ((int)(((byte)(23)))));
+            this.txtTaiKhoan.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.txtTaiKhoan.Location = new System.Drawing.Point(25, 238);
+            this.txtTaiKhoan.Name = "txtTaiKhoan";
+            this.txtTaiKhoan.Size = new System.Drawing.Size(393, 27);
+            this.txtTaiKhoan.TabIndex = 1;
+            // 
+            // txtMatKhau
+            // 
+            this.txtMatKhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtMatKhau.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(18)))), ((int)(((byte)(23)))));
+            this.txtMatKhau.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.txtMatKhau.Location = new System.Drawing.Point(25, 303);
+            this.txtMatKhau.Name = "txtMatKhau";
+            this.txtMatKhau.Size = new System.Drawing.Size(393, 27);
+            this.txtMatKhau.TabIndex = 2;
+            // 
+            // lblTaiKhoan
+            // 
+            this.lblTaiKhoan.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTaiKhoan.AutoSize = true;
+            this.lblTaiKhoan.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaiKhoan.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTaiKhoan.Location = new System.Drawing.Point(22, 213);
+            this.lblTaiKhoan.Name = "lblTaiKhoan";
+            this.lblTaiKhoan.Size = new System.Drawing.Size(87, 21);
+            this.lblTaiKhoan.TabIndex = 0;
+            this.lblTaiKhoan.Text = "Tài khoản:";
+            // 
+            // lblMatKhau
+            // 
+            this.lblMatKhau.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblMatKhau.AutoSize = true;
+            this.lblMatKhau.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMatKhau.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblMatKhau.Location = new System.Drawing.Point(22, 279);
+            this.lblMatKhau.Name = "lblMatKhau";
+            this.lblMatKhau.Size = new System.Drawing.Size(86, 21);
+            this.lblMatKhau.TabIndex = 0;
+            this.lblMatKhau.Text = "Mật khẩu:";
+            // 
+            // pbHinh
+            // 
+            this.pbHinh.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pbHinh.Image = ((System.Drawing.Image)(resources.GetObject("pbHinh.Image")));
+            this.pbHinh.Location = new System.Drawing.Point(142, 40);
+            this.pbHinh.Name = "pbHinh";
+            this.pbHinh.Size = new System.Drawing.Size(143, 134);
+            this.pbHinh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbHinh.TabIndex = 2;
+            this.pbHinh.TabStop = false;
+            // 
+            // lblTieuDe
+            // 
+            this.lblTieuDe.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTieuDe.AutoSize = true;
+            this.lblTieuDe.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTieuDe.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblTieuDe.Location = new System.Drawing.Point(90, 177);
+            this.lblTieuDe.Name = "lblTieuDe";
+            this.lblTieuDe.Size = new System.Drawing.Size(267, 25);
+            this.lblTieuDe.TabIndex = 0;
+            this.lblTieuDe.Text = "SHOP QUẢN LÝ GIÀY DÉP";
+            // 
+            // btnDangNhap
+            // 
+            this.btnDangNhap.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDangNhap.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnDangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangNhap.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDangNhap.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDangNhap.Location = new System.Drawing.Point(26, 391);
+            this.btnDangNhap.Name = "btnDangNhap";
+            this.btnDangNhap.Size = new System.Drawing.Size(392, 41);
+            this.btnDangNhap.TabIndex = 4;
+            this.btnDangNhap.Text = "Đăng nhập";
+            this.btnDangNhap.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(18)))), ((int)(((byte)(23)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(448, 12);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(46, 41);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // timeOut
+            // 
+            this.timeOut.Tick += new System.EventHandler(this.timeOut_Tick);
+            // 
+            // chkLuuThongTin
+            // 
+            this.chkLuuThongTin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.chkLuuThongTin.AutoSize = true;
+            this.chkLuuThongTin.Location = new System.Drawing.Point(105, 347);
+            this.chkLuuThongTin.Name = "chkLuuThongTin";
+            this.chkLuuThongTin.Size = new System.Drawing.Size(222, 25);
+            this.chkLuuThongTin.TabIndex = 3;
+            this.chkLuuThongTin.Text = "Lưu thông tin đăng nhập";
+            this.chkLuuThongTin.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(506, 477);
+            this.Controls.Add(this.chkLuuThongTin);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDangNhap);
+            this.Controls.Add(this.pbHinh);
+            this.Controls.Add(this.txtMatKhau);
+            this.Controls.Add(this.txtTaiKhoan);
+            this.Controls.Add(this.lblMatKhau);
+            this.Controls.Add(this.lblTaiKhoan);
+            this.Controls.Add(this.lblTieuDe);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Thông tin đăng nhập";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.pbHinh)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTaiKhoan;
+        private System.Windows.Forms.TextBox txtMatKhau;
+        private System.Windows.Forms.Label lblTaiKhoan;
+        private System.Windows.Forms.Label lblMatKhau;
+        private System.Windows.Forms.PictureBox pbHinh;
+        private System.Windows.Forms.Label lblTieuDe;
+        private System.Windows.Forms.Button btnDangNhap;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer timeOut;
+        private System.Windows.Forms.CheckBox chkLuuThongTin;
+    }
+}
+
